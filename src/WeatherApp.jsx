2ce5e78616e93bdc60d25a4eb699e5368682871e
Gian Barrantes <129077296+Gbarrantes25@users.noreply.github.com>
2023-09-07 19:20:15 -0500

    Add files via upload

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+
+const API_KEY = "51fe76011943409ecade9e17caf2248b";
+const diffKelvin = 273.15
+
+
+export const WeatherApp = () => {
+
+    const [ciudad, setCiudad] = useState("");
+    const [dataWeather, setDataWeather] = useState(null)
+
+    const handleCambioCiudad = ({ target }) => {
+        const { value } = target;
+        setCiudad(value.trimStart());
+        
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (ciudad.length > 0) fethWeather(ciudad.trim());
+        
+    };
+
+
+
+    const fethWeather = async (ciudad) => {
+
+        try {
+            // URL API = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+            const data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+                                            ${ciudad}&appid=${API_KEY}`)).json();
+
+            data.name ? setDataWeather(data) : setDataWeather(null)
+
+        } catch (error) {
+            setDataWeather(null)
+        }
+
+    }
+
+    return (
+        <div className='container'>
+
+            <img className="img-app" 
+                src="https://www.pngall.com/wp-content/uploads/11/Weather-Background-PNG.png"
+            ></img>
+            
+
+            <form onSubmit={handleSubmit}>
+
+                <input
+                    type='text'
+                    value={ciudad}
+                    onChange={handleCambioCiudad}
+                    placeholder='Escriba una ciudad'
+                />
+
+                <button type='submit'>
+                    Buscar
+                </button>
+
+            </form>
+
+            {dataWeather === null
+                ? <div>
+                    <h2>Ingrese ciudad</h2>
+                </div>
+                : <div>
+                    <h1>{dataWeather.name} - {dataWeather.sys.country}</h1>
+                    <h2>Temperatura: {parseInt(
+                        (dataWeather.main.temp) - diffKelvin)} °C
+                    </h2>
+                    <p>Condición meteorológica: {dataWeather.weather[0]
+                                    .description}
+                    </p>
+                    <img className="icon" 
+                        src={`https://openweathermap.org/img/wn/${dataWeather
+                        .weather[0].icon}@2x.png`}>    
+                    </img>
+                </div>
+            }
+
+        </div>
+    )
+}
